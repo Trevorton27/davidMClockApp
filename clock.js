d1e503f7cbfce793bc5ca5c177b6d5b8ca2ea6f8
Trevor Mearns <spiral272@gmail.com>
2021-03-23 13:48:38 +0900

    ready for code review approval

--- clock.js
+++ clock.js
@@ -1,77 +1,84 @@
-function masterTime() {
-    let today = new Date();
-    const dateDisplay = document.getElementById("date");
-    const timeDisplay = document.getElementById("time")
+let amPm;
+function displayDate() {
+  const today = new Date();
+  const dateDisplay = document.getElementById('date');
 
+  const date = today.getDate();
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
 
+  const day = days[today.getDay()];
 
-    let dd = String(today.getDate()).padStart(2, '0');
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let day = days[today.getDay()];
-    function end(dd) {
-        let dayEnding;
-        if (dd == "01" || dd == "21" || dd == "31") {
-            dayEnding = "st"
-        } else if (dd == "02" || dd == "22") {
-            dayEnding = "nd"
-        } else if (dd == "03" || dd == "23") {
-            dayEnding = "rd"
-        } else {
-            dayEnding = "th"
-        }
-        return dayEnding;
+  const month = today.getMonth();
+  const monthDict = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
 
-    };
+  const year = today.getFullYear();
 
-    let month = String(today.getMonth()).padStart(2, '0');
-    let monthDict = {
-        "00": "January",
-        "01": "February",
-        "02": "March",
-        "03": "April",
-        "04": "May",
-        "05": "June",
-        "06": "July",
-        "07": "August",
-        "08": "September",
-        "09": "October",
-        "10": "November",
-        "11": "December"
-    };
+  const now = `${day}, ${monthDict[month]} ${convertToOrdinal(date)} ${year} `;
 
-    let year = today.getFullYear();
-
-    let now = day + ", " + monthDict[month] + ' ' + dd + end(dd) + ' ' + year;
-    dateDisplay.textContent = now;
-
-
-    let hour = today.getHours();
-    function nonMilitary(x) {
-        let hours;
-        if (x <= 12) {
-            hours = x
-        } else if (x > 12) {
-            hours = x - 12
-        } return String(hours);
-    };
-    function dayNight(hour) {
-        let amPM;
-        if (hour < 12) {
-            amPM = "AM"
-        } else if (hour >= 12) {
-            amPM = "PM"
-        } return amPM;
-    };
-
-
-    let minute = String(today.getMinutes()).padStart(2, "0");
-
-    let second = String(today.getSeconds()).padStart(2, "0")
-
-
-
-    let time = nonMilitary(hour) + " : " + minute + " : " + second + ' ' + dayNight(hour);
-    timeDisplay.textContent = time;
+  dateDisplay.textContent = now;
 }
 
-setInterval(masterTime, 1000)
+function displayTime() {
+  const timeDisplay = document.getElementById('time');
+  const time = new Date();
+  let hour = dayNight(time.getHours());
+  const minute = addLeadingZero(time.getMinutes());
+  const second = addLeadingZero(time.getSeconds());
+
+  const timeString = `${dayNight(hour)}:${minute}:${second} ${amPm}`;
+
+  timeDisplay.textContent = timeString;
+}
+
+function convertToOrdinal(number) {
+  if (number < 10 || number > 20) {
+    switch (number % 10) {
+      case 1:
+        return number + 'st';
+      case 2:
+        return number + 'nd';
+      case 3:
+        return number + 'rd';
+    }
+  }
+  return number + 'th';
+}
+
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
+}
+
+function dayNight(hour) {
+  const isAm = hour < 12 || hour === 0;
+  amPm = isAm ? 'AM' : 'PM';
+  hour = hour >= 13 ? hour - 12 : hour;
+
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
+
+displayTime();
+displayDate();
+
+setInterval(displayTime, 1000);
